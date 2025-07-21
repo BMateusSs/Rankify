39af655b5301a879c15d769ee780db2522f8d2ee
BMateusSs <bmdss0810@gmail.com>
2025-07-21 15:04:04 -0300

    Função para calcular intervalos semanais

---
+++ backend/src/utils/get_valid_dates.py
@@ -0,0 +1,26 @@
+from datetime import datetime, date, timedelta
+
+def first_friday(register_date):
+    days_to_go_back = (register_date.weekday() - 4) % 7
+    date = register_date - timedelta(days=days_to_go_back)
+    return date
+
+def date_intervals(register_date, today):
+    friday = first_friday(register_date)
+    weeks = []
+    
+    while True:
+        thursday = friday + timedelta(days=6) 
+        if thursday >= today:
+            break
+        
+        weeks.append((friday, thursday))
+        friday = thursday + timedelta(days=1)
+        
+        
+    print(weeks)
+    
+if __name__ == '__main__':
+    thursday = date(2025, 6, 29)
+    today = date(2025, 7, 21)
+    date_intervals(thursday, today)
