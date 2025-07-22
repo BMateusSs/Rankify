e5b876a72fd07a333ef0de98c697b53c1111ef5d
BMateusSs <bmdss0810@gmail.com>
2025-07-21 21:32:35 -0300

    Configuração do database

--- backend/config.py
+++ backend/config.py
@@ -8,3 +8,8 @@ class Config:
     LASTFM_SECRET_KEY = os.getenv('LASTFM_SECRET_KEY')
     LASTFM_USER = os.getenv('LASTFM_USER')
     LASTFM_URL = 'http://ws.audioscrobbler.com/2.0/'
+
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_NAME = os.getenv('DB_NAME')
