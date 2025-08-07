b338c57ad53b3a51bd6b3a5c6845b074d7566567
BMateusSs <bmdss0810@gmail.com>
2025-08-07 10:15:47 -0300

    Incluindo novo blueprint ao app

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from src.app.charts import charts_bp
+from src.app.emblems import emblems_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(charts_bp)
+    app.register_blueprint(emblems_bp)
     return app
